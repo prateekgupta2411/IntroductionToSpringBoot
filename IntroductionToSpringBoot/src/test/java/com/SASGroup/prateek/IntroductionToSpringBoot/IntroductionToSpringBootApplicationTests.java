6cdef4afc804975b26d21a062ffbe22f118f62a5
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2024-11-12 22:00:15 +0530

    SpringBoot

---
+++ IntroductionToSpringBoot/src/test/java/com/SASGroup/prateek/IntroductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SASGroup.prateek.IntroductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
